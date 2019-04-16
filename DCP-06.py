81f58bd58d6375c0d910957058cf8d21d513b22a
Krishna Pandey <Krishna.Pandey@nectechnologies.in>
2019-04-16 16:52:37 +0530

    given an array of wall, tell water traped optimized solution

--- DCP-06.py
+++ DCP-06.py
@@ -35,5 +35,30 @@ print(waterRemain(wall,n))
 more optimized solution is instead of taking array of left and right max tower, we will take variable
 """
 
+def waterTraped(arr,n):
+	water=0
+	left_max=0
+	right_max=0
+	low=0
+	high=n-1
 
+	while(low <=high):
+		if(arr[low]< arr[high]):
+			if(arr[low] >left_max):
+				left_max=arr[low]
+			else:
+				water +=left_max -arr[low]
+			low +=1
+		else:
+			if(arr[high] > right_max):
+				right_max = arr[high]
+			else:
+				water +=right_max-arr[high]
+			high -=1
+	return water
+
+
+wall = [3,0,0,2,0,4]
+n=len(wall)
+print(waterTraped(wall,n))
 
